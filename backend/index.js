2c519197e2d3df8e34944de2a9d07e5f51542d57
tino Mukaro <tino.mukaro3@gmail.com>
2025-05-02 21:19:42 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const Admin = require("./models/admin");
-const TrafficOfficer = require("./models/trafficOfficer");
+
 
 const adminRoutes = require("./routes/adminRoutes");
 const trafficOfficerRoutes = require("./routes/trafficOfficerRoutes");
 const offenseRoutes = require("./routes/offenseRoutes");
 const offendersRoutes = require("./routes/offendersRoutes");
-const startPenaltyPointsCron = require("./config/cronJobs");
+const { startPenaltyPointsCron } = require("./config/cronJobs");
+const { executePenaltyPointsJob } = require("./config/cronJobs");
+const offenderLogsRoutes = require('./routes/offenderLogsRoutes');
+
 
 
 
@@ -16,26 +18,37 @@ const app = express();
 
 // CORS Configuration
 const corsOptions = {
-  origin: "http://localhost:5173", 
-  credentials: true, 
+  origin: function (origin, callback) {
+    callback(null, true); // Allow all origins
+  },
+  credentials: true,
 };
 app.use(cors(corsOptions));
 
 app.use(express.json());
 
-// Routes
+//Routes
 app.use("/api/admins", adminRoutes);
 app.use("/api/officers", trafficOfficerRoutes);
 app.use("/api/offenses", offenseRoutes);
 app.use("/api/offenders", offendersRoutes);
+app.use('/api', offenderLogsRoutes);
 
-app.get("/", (req, res) => {
-    res.send("TOMS Backend Running");
+
+
+
+app.get("/api/test-cron", async (req, res) => {
+  try {
+    const result = await executePenaltyPointsJob(); // Direct execution
+    res.json({ success: true, message: result.message });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
 });
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 
   startPenaltyPointsCron();
