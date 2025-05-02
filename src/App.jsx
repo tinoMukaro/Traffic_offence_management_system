919d1a2745e6c2f24e569245a5db350e4c48446b
tino Mukaro <tino.mukaro3@gmail.com>
2025-05-02 21:10:04 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,52 @@
-import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/navbar";
 import Home from "./pages/Home";
-import Admin from "./pages/Admin";
-import Officer from "./pages/Officer";
+import Navbar from "./componets/NavBar";
+import Footer from "./componets/Footer";
 import Login from "./pages/Login";
+import AdminDashboard from "./pages/AdminDashboard";
 import AdminLogin from "./pages/AdminLogin";
 import OfficerLogin from "./pages/OfficerLogin";
-import AdminDashboard from "./pages/AdminDashboard";
 import TrafficOfficerDashboard from "./pages/OfficerDashBoard";
-import OffenceLogin from "./pages/OffenceLogin"
-import { fetchData } from "./api";
 import AdminRegister from "./pages/AdminRegister";
-import TrafficOfficerRegister from "./pages/TrafficOfficerRegister";
-import ViewOffenses from "./pages/ViewOffence";
 import { ManageTrafficOfficers } from "./pages/ManageTrafficOfficers";
-import Offender from "./pages/offenders";
+import ViewOffenses from "./pages/ViewOffence";
 import UpdateOffense from "./pages/UpdateOffense";
+import Offender from "./pages/Offenders";
+import OffenceLogin from "./pages/OffenceLogin";
+import TrafficOfficerRegister from "./pages/TrafficOfficerRegister";
+import About from "./pages/About";
+import Contacts from "./pages/Contacts";
+import PenaltyPointsLogs from "./pages/PenaltyPOintLogs";
+
 
 
 const App = () => {
-  
-
   return (
     <Router>
-      <Navbar />
-      <div className="container mt-4">
+       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} /> 
+        <Route path="/admin-login" element={<AdminLogin />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/officer-login" element={<OfficerLogin />} />
+        <Route path="/officer-dashboard" element={<TrafficOfficerDashboard />} />
+        <Route path="/admin-register" element={< AdminRegister />} />
+        <Route path="/admin-users" element={< ManageTrafficOfficers />} />
+        <Route path="/view-offenses" element={< ViewOffenses />} />
+        <Route path="/update-offense" element={< UpdateOffense />} />
+        <Route path="/offenders" element={< Offender />} />
+        <Route path="/log-offense" element={<OffenceLogin />} /> 
+        <Route path="/officer-register" element={< TrafficOfficerRegister />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/penalty-points" element={<PenaltyPointsLogs />} />
         
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/officer" element={<Officer />} />
-          <Route path="/login" element={<Login />} /> 
-          <Route path="/admin-login" element={<AdminLogin />} />
-          <Route path="/officer-login" element={<OfficerLogin />} />
-          <Route path="/admin-dashboard" element={<AdminDashboard />} />
-          <Route path="/officer-dashboard" element={<TrafficOfficerDashboard />} />
-          <Route path="/log-offense" element={<OffenceLogin />} />
-          <Route path="/admin-register" element={< AdminRegister />} />
-          <Route path="/officer-register" element={< TrafficOfficerRegister />} />
-          <Route path="/view-offenses" element={< ViewOffenses />} />
-          <Route path="/admin-users" element={< ManageTrafficOfficers />} />
-          <Route path="/offenders" element={< Offender />} />
-          <Route path="/update-offense" element={< UpdateOffense />} />
-          
 
-        </Routes>
-      </div>
+        
+      </Routes>
+      <Footer />
     </Router>
   );
-};
-
+}
 export default App;
